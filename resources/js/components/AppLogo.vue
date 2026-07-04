7646beadbfc1dc0f85a0cd3fb44846bd8d027083
Paperbuddymk <paperbuddy.mk@gmail.com>
2026-07-04 16:35:18 +0200

    feat: replace SVG logo with FinanceBuddy.mk logo image

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,10 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-</script>
-
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
-    </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >FinanceBuddy.mk</span
-        >
+    <img
+        src="https://i.imgur.com/fEiVbXj.png"
+        alt="FinanceBuddy.mk"
+        class="h-8 w-auto object-contain"
+    />
+    <div class="ml-1 grid flex-1 text-left text-sm leading-tight">
+        <span class="truncate font-semibold">FinanceBuddy.mk</span>
     </div>
 </template>
